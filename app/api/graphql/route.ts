ed3e863a98fb4a25526afd8219d1185a9461da94
MykyK <kaluzhniynikita@gmail.com>
2026-01-28 16:25:03 +0100

    Fix deploy

--- app/api/graphql/route.ts
+++ app/api/graphql/route.ts
@@ -2,14 +2,57 @@ import { prisma } from "@/lib/prisma";
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 
-const typeDefs = `
-  type User { id: String! email: String! name: String }
-  type Query { users: [User!]! }
+const typeDefs = /* GraphQL */ `
+  type User {
+    id: String!
+    email: String!
+    name: String
+  }
+
+  type Movie {
+    id: Int!
+    title: String!
+    posterUrl: String
+    year: Int
+  }
+
+  type Swipe {
+    id: String!
+    value: String!
+    userId: String!
+    movieId: Int!
+    sessionId: String!
+  }
+
+  type Match {
+    id: String!
+    movieId: Int!
+    sessionId: String!
+  }
+
+  type Query {
+    users: [User!]!
+    movies: [Movie!]!
+    swipes: [Swipe!]!
+    matches: [Match!]!
+  }
 `;
 
-const resolvers = { Query: { users: () => prisma.user.findMany() } };
+const resolvers = {
+  Query: {
+    users: () => prisma.user.findMany(),
+    movies: () => prisma.movie.findMany(),
+    swipes: () => prisma.swipe.findMany(),
+    matches: () => prisma.match.findMany(),
+  },
+};
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-export const GET = async (req: Request) => startServerAndCreateNextHandler(server)(req);
-export const POST = async (req: Request) => startServerAndCreateNextHandler(server)(req);
+export const GET = async (request: Request) => {
+  return startServerAndCreateNextHandler(server)(request);
+};
+
+export const POST = async (request: Request) => {
+  return startServerAndCreateNextHandler(server)(request);
+};
